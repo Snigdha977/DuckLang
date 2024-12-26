e893d3eaaeb1d53987afc6325244349b3b5b2d24
Puskar-Roy <puskarroy600@gmail.com>
2024-12-26 22:14:28 +0530

    fear : env

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+name = os.getenv("NAME", "False")  
+
 def greet(name):
     print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
-    greet("Duck Lang")
+    greet(name)
