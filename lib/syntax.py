5e423f9758f26c69105733a2130a3418da547e59
Dhritikrishna Tripathi <Dhritikrishna123@users.noreply.github.com>
2024-12-27 20:00:25 +0530

    Added all the variables that came to mind in the python style such that the lexer can recognize the data type of the literal

--- lib/syntax.py
+++ lib/syntax.py
@@ -5,9 +5,12 @@ load_dotenv()
 
 name = os.getenv("NAME", "UNKNOWN")
 print_command = os.getenv("PRINT_COMMAND", "UNKNOWN")
+var_command = os.getenv("VARIABLE_DECLARE", "UNKNOWN")
 
 
 syntax = {
     "name": name,
     "PRINT_COMMAND": print_command,
+    "VARIABLE_DECLARE": var_command,
 }
+
--- main.py
+++ main.py
@@ -5,13 +5,30 @@ from src.lexer import lexer
 
 
 
-source_code = 'quack("Hello World")'
+# Test code
+source_code = '''
+quack("Hello World")
+duck my_int = 42
+duck my_float = 3.14
+duck my_bool = True
+'''
 
 def run_lexer(source_code):
     '''Call the lexer to tokenize the source code'''
     tokens = lexer(source_code)
+    
+    # Print header
+    print(f"{'Token Type':<25} {'Value':<30} {'Position'}")
+    print("="*80)  # Separator line
+    
+    # Print each token in a well-formatted way
     for token in tokens:
-        print(f"Token Type: {token['type']}, Value: {token['value']}, Position: {token['position']}")
-
+        token_type = token['type']
+        value = token['value']
+        position = token['position']
+        
+        # Print token info with proper formatting
+        print(f"{token_type:<25} {str(value):<30} {position}")
+    
 if __name__ == "__main__":
     run_lexer(source_code)
