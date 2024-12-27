b68804a4b8db8917c16c47950d0b4052e1273c74
Dhritikrishna Tripathi <Dhritikrishna123@users.noreply.github.com>
2024-12-27 21:31:35 +0530

    feat(dict-list): add dictionary and list handling system

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from src.lexer import lexer
 # Test code
 source_code = '''
 quack("Hello World")
+my_list = [hello, "hello" , 87]
+my_dict = {"key": "value", "age": 25}
 duck my_int = 42
 duck my_float = 3.14
 duck my_bool = True
